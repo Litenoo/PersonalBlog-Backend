08b200ecd34916bab83d6abee42ee2ba5c670fcd
lukasz <lukaszcholewka@outlook.com>
2025-06-22 22:05:33 +0200

    Add unit tests, need to commit before change from spyOn to Mocking the prismaClient

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,24 +1,47 @@
 import { PrismaClient } from "@prisma/client";
-import type { Post } from "@prisma/client";
+import type { Post, Tag } from "@prisma/client";
+import logger from "../utils/logger";
 
 export default class DatabaseService {
 	constructor(private prisma: PrismaClient = new PrismaClient()) {
 		this.prisma = prisma;
 	}
 
-	async getPostById(params: { postId: number, withContent: boolean })
-		: Promise<{ post?: Post | null, errorCode?: string }> {
+	async getPostById(params: { postId: number; withContent: boolean }): Promise<
+		{ post?: Post | null; errorCode?: string } |
+		{ post?: Omit<Post, "content"> | null; errorCode?: string }
+	> {
 		try {
-			const post: Post | null = await this.prisma.post.findUnique({
+			const post = await this.prisma.post.findUnique({
 				where: {
 					id: params.postId,
 					published: true,
 				},
+				select: params.withContent
+					? { id: true, title: true, content: true, published: true, createdAt: true, updatedAt: true }
+					: { id: true, title: true, published: true, createdAt: true, updatedAt: true },
 			});
-			console.log("Post ID : ", params.postId, "Post fetched:", post);
-			return { post };
+
+			if (!post) {
+				return { post: null, errorCode: "not_found" };
+			}
+			return { post, errorCode: undefined };
+
 		} catch (error) {
+			logger.error(`Error fetching post with ID ${params.postId}:`, error);
 			return { errorCode: "critical_error" };
 		}
+	}
+
+	async insertPost(
+		params: {
+			title: string,
+			content: string,
+			tags: string[], //if no tags exist decide what to do
+			published: boolean,
+		}): Promise<
+			void
+		> {
+
 	}
 };
--- tests/unit/post.test.ts
+++ tests/unit/post.test.ts
@@ -1,40 +1,91 @@
-import { PrismaClient } from "@prisma/client";
 import { describe, expect, test, vi, beforeEach } from 'vitest';
 import DbService from '../../src/services/database.service';
+
+vi.mock('@prisma/client', () => ({
+	PrismaClient: vi.fn().mockImplementation(() => ({
+		post: {
+			create: vi.fn(),
+		},
+	})),
+}));
+
+import { PrismaClient } from "@prisma/client";
+
 
 describe(`getPostById`, async () => {
 	let prisma: PrismaClient;
 	let db: DbService;
 
+	const postNoContent = {
+		id: 1,
+		title: 'Test Post',
+		tags: [
+			{ id: 1, title: 'tag1' },
+			{ id: 2, title: 'tag2' },
+		],
+		published: true,
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	}
+
+	const postWithContent = {
+		...postNoContent,
+		content: 'This is a test post content.',
+	}
+
 	beforeEach(() => {
 		vi.clearAllMocks();
-		prisma = new PrismaClient(); // Not sure if it has to be the same which is defined in the tested class
+		prisma = new PrismaClient();
 		db = new DbService(prisma);
 	});
 
-	test(`should return post with content`, async () => {
-
-		const fakePost = {
-			id: 1,
-			title: 'Test Post',
-			content: 'This is a test post.',
-			tags: [
-				{ id: 1, title: 'tag1' },
-				{ id: 2, title: 'tag2' },
-			],
-			published: true,
-			createdAt: new Date(),
-			updatedAt: new Date(),
-		}
-
-		vi.spyOn(prisma.post, 'findUnique').mockResolvedValue(fakePost as any);
+	test(`Should return post with content`, async () => {
+		vi.spyOn(prisma.post, 'findUnique').mockResolvedValue(postWithContent as any);
 
 		const result = await db.getPostById({ postId: 1, withContent: true });
 
 		expect(prisma.post.findUnique).toHaveBeenCalledWith({
 			where: { id: 1, published: true },
+			select: { content: true, id: true, title: true, published: true, createdAt: true, updatedAt: true },
 		});
 
-		expect(result).toEqual({ post: fakePost });
+		expect(result).toEqual({ post: postWithContent });
+	});
+
+	test(`Should return post without content`, async () => {
+		vi.spyOn(prisma.post, 'findUnique').mockResolvedValue(postNoContent as any);
+
+		const result = await db.getPostById({ postId: 1, withContent: false });
+
+		expect(prisma.post.findUnique).toHaveBeenCalledWith({
+			where: { id: 1, published: true },
+			select: { id: true, title: true, published: true, createdAt: true, updatedAt: true },
+		});
+
+		expect(result).toEqual({ post: postNoContent });
+	})
+});
+
+
+describe("insertPost", async () => {
+	// It is going to always return code message if post was inserted correctly
+	let prismaMock: PrismaClient;
+	let db: DbService;
+
+	beforeEach(() => {
+		vi.clearAllMocks();
+		prismaMock = new PrismaClient();
+		db = new DbService(prismaMock);
+	});
+
+	test("Should insert post to the database", async () => {
+		const mockPost = {
+			id: 1,
+			title: "SomeTitle",
+			content: "Some content very long",
+			published: true,
+			tags: ["tag1"],
+		}
+		prismaMock.post.create.mockResolvedValue()
 	});
 });
